b565c94a62dd992b4b149ff5e0c86b19c03d27ec
jianyanbo <97041634@qq.com>
2021-12-25 08:40:15 +0800

    定义应用标志组件 AppLogo

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,3 +1,14 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
+  <AppLogo />
 </template>
+
+<script>
+import AppLogo from '@/app/components/app-logo.vue';
+
+export default {
+  components: {
+    AppLogo,
+  },
+};
+</script>
---
+++ src/app/components/app-logo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="appLogoClasses">
+    <img src="http://localhost:3000/files/6/serve" alt="" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppLogo',
+
+  /**
+   * 属性
+   */
+  props: {},
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    appLogoClasses() {
+      return ['app-logo'];
+    },
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped>
+@import './styles/app-logo.css';
+</style>
