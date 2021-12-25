87224b712671021e0adb3e21f1a44fac79b172b8
jianyanbo <97041634@qq.com>
2021-12-25 08:53:17 +0800

    定义应用图标组件 AppIcon

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
   <AppLogo />
+  <AppIcon />
 </template>
 
 <script>
 import AppLogo from '@/app/components/app-logo.vue';
+import AppIcon from '@/app/components/app-icon.vue';
 
 export default {
   components: {
     AppLogo,
+    AppIcon,
   },
 };
 </script>
